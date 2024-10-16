2d4b566f33888a5d7b26e2a1e9cba48e264c1a21
Pawel Galczynski <pgalczynski@student.agh.edu.pl>
2024-10-16 22:30:23 +0200

    for good sleep some coding

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -11,6 +11,14 @@ public class World {
         MoveDirection[] directions = OptionsParser.parse(args);
         run(directions);
         System.out.println("system zakonczyl dzialanie");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+
     }
 
     public static void run(MoveDirection[] directions) {
