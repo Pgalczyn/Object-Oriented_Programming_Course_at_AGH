4dda95201e0805b66ade4fae396541d7cfdb39db
Pawel Galczynski <pgalczynski@student.agh.edu.pl>
2024-11-03 19:03:12 +0100

    it works finally

--- oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -1,5 +1,7 @@
 package agh.ics.oop.model;
 
+import agh.ics.oop.model.util.MapVisualizer;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,27 +20,47 @@ public class RectangularMap implements WorldMap {
     @Override
     public boolean place(Animal animal) {
 
-
+        if(canMoveTo(animal.getLocation())){
+            animals.put(animal.getLocation(), animal);
+            return true;
+        }
+        return false;
 
     }
 
     @Override
     public void move(Animal animal, MoveDirection direction) {
 
+        if (animals.containsValue(animal)) {
+
+           Vector2d location = animal.getLocation();
+           animal.move(direction,this);
+            animals.remove(location);
+            animals.put(animal.getLocation(), animal);
+
+        }
+
     }
 
     @Override
     public boolean isOccupied(Vector2d position) {
-        return false;
+        return objectAt(position) != null;
     }
 
     @Override
     public Animal objectAt(Vector2d position) {
-        return null;
+        return animals.get(position);
     }
 
     @Override
     public boolean canMoveTo(Vector2d position) {
-        return false;
+        return position.precedes(new Vector2d(width, height)) && position.follows(new Vector2d(0,0)) && !isOccupied(position);
+    }
+
+    @Override
+    public String toString() {
+        MapVisualizer mapa = new MapVisualizer(this);
+        return mapa.draw(new Vector2d(0,0),new Vector2d(width,height));
+
     }
 }
